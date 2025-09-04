cd0cde7fd59fbead34061c9976c4da6ad857b81a
Samuel Skean <sskean2@uic.edu>
2025-09-04 07:47:53 -0500

    Updated Discord URLs to not expire.
    ...according to the Discord UI they won't expire, that is.
    I trust it :).

--- src/qrCodeUrls.ts
+++ src/qrCodeUrls.ts
@@ -1,5 +1,5 @@
 export const qrCodeUrls = {
-    "acmDiscord": "https://discord.gg/aeN2KPJA",
-    "lugDiscord": "https://discord.gg/GX8ghFch",
+    "acmDiscord": "https://discord.gg/uvP6wN3Yar",
+    "lugDiscord": "https://discord.gg/TCUc63mSSq",
     "appRepo": "https://github.com/acm-uic/simple-ts-clock"
 };
